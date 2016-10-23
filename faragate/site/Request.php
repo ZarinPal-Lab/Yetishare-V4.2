5a0bfa0f8ceaaddbbd4f218113008d649d346b17
sardarn <sardarn84@yahoo.com>
2016-10-23 11:56:13 -0700

    Update Request.php

--- faragate/site/Request.php
+++ faragate/site/Request.php
@@ -96,12 +96,16 @@ if ($order)
 		
 		if ( $result->Status == 100){
 			
-			include(SITE_TEMPLATES_PATH.'/upgrade.html');	
-		
-			$Payment_URL =  'https://www.zarinpal.com/pg/StartPay/' . $result->Authority.'/ZarinGate';
+		include(SITE_TEMPLATES_PATH.'/upgrade.html');	
 
-			if ( ! headers_sent() ) header('Location: ' . $Payment_URL ); exit;
-			die('<script type="text/javascript">window.location="' .$Payment_URL. '";</script>'); exit;
+			echo '<script type="text/javascript" src="https://cdn.zarinpal.com/zarinak/v1/checkout.js"></script>
+				<script>
+				Zarinak.setAuthority( ' . $result->Authority . ');
+				Zarinak.open();
+				</script>';
+				
+			/*if ( ! headers_sent() ) header('Location: ' . $Payment_URL ); exit;
+			die('<script type="text/javascript">window.location="' .$Payment_URL. '";</script>'); exit;*/
 			
 		}
 		else {
